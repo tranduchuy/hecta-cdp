a7db3412d7578f9a5696d7c24c993163803a5b5b
Long Nguyen <nguyenbinhlong92@gmail.com>
2018-12-23 10:52:00 +0700

    Api get transaction history

--- source/src/controllers/transaction-history/transaction-history.controller.js
+++ source/src/controllers/transaction-history/transaction-history.controller.js
@@ -2,9 +2,11 @@ const GlobalConstant = require('../../constants/global.constant');
 const HttpCodeConstant = require('../../constants/http-code.constant');
 const log4js = require('log4js');
 const logger = log4js.getLogger(GlobalConstant.LoggerTargets.Controller);
-const URService = require('./transaction-history.service');
+const TransHisService = require('./transaction-history.service');
+const RequestService = require('../../services/request.service');
 const AJV = require('../../core/ajv');
 const ctrlNm = 'TransactionHistoryController';
+const transType = require('../../constants/transaction-type.constant');
 
 // models
 /**
@@ -12,13 +14,27 @@ const ctrlNm = 'TransactionHistoryController';
  */
 const TransactionModel = require('../../models/transaction.model');
 
-const listMyTransactionHistory = async (req, res, next) => {
-  logger.info('TransactionHistory::listMyTransactionHistory::called');
-  try {
-    // TODO: get list my transaction history
-  } catch (e) {
+// schemas
+const getListMyTransactionHistory = require('./validation-schemas/list-my-transaction.schema');
 
-  }
+const listMyTransactionHistory = async (req, res, next) => {
+    logger.info('TransactionHistory::listMyTransactionHistory::called');
+    try {
+        const errors = AJV(getListMyTransactionHistory, req.query);
+        if (errors.length !== 0) {
+            return res.json({
+                status: HttpCodeConstant.Error,
+                messages: errors,
+                data: {meta: {}, entries: []}
+            });
+        }
+
+        const paginationOptions = RequestService.extractPaginationCondition(req);
+        const optionQuery = Object.assign({}, paginationOptions, TransHisService.mapQueryToValidObjectSort(req.query));
+    } catch (e) {
+        logger.error(`${ctrlNm}::listMyTransactionHistory::error`, e);
+        return next(e);
+    }
 };
 
 module.exports = {
